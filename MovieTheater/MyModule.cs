a89cb4126d2b61a7738cb9bbe9153c2c769f8393
haiconga263 <trandinhhiephtd@gmail.com>
2016-05-14 00:33:03 +0700

    Mater Project
    Thiết kế sơ sơ giao diện

---
+++ MovieTheater/MyModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieTheater.Module
+{
+    class MyModule
+    {
+        public static void fixColumnSize(DataGridView dgv)
+        {
+            int count = dgv.ColumnCount;
+            for (int i = 0; i <= count - 1; i++)
+            {
+                dgv.Columns[i].Width = dgv.Width / count;
+            }
+        }
+
+    }
+}
---
+++ MovieTheater/NumTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieTheater.Module
+{
+    class NumTextBox : TextBox
+    {
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b')
+            {
+                if (e.KeyChar < 0x30 || e.KeyChar > '9')
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
